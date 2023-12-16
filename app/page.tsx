151c6d0f854bb432ff2c01db256a069144c466a2
Remco Lakens <remco.lakens@gmail.com>
2023-12-16 14:23:26 +0100

    feat: add new unit tests

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-				<h2 className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					Get started by editing&nbsp;
 					<code className="font-mono font-bold">app/page.tsx</code>
-				</h2>
+				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 					<a
 						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -15,7 +15,11 @@ export default function Home() {
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						By <Vercel className="dark:invert w-24 h-6 text-black" />
+						By{' '}
+						<Vercel
+							className="dark:invert w-24 h-6 text-black"
+							data-testid="next-svg"
+						/>
 					</a>
 				</div>
 			</div>
@@ -55,7 +59,7 @@ export default function Home() {
 						</span>
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Learn about Next.js in an interactive course with&nbsp;quizzes!
+						Learn about Next.js in an interactive course with quizzes!
 					</p>
 				</a>
 
