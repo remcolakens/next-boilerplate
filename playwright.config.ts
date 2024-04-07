26992939e81f2085bdcf3a20943a551cf57bbe0d
Remco Lakens <remco.lakens@gmail.com>
2024-04-07 14:47:22 +0200

    feat: add e2e tests

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testMatch: 'e2e/*.spec.ts',
+	testDir: 'e2e',
+	reporter: [['html', { open: 'never', outputFolder: 'e2e/report' }]],
+	outputDir: 'e2e/test-results/',
+
+	workers: process.env.CI ? 1 : undefined,
+	fullyParallel: true,
+
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 3 : 0,
+
+	use: {
+		baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? 'http://localhost:3000',
+		trace: 'on-first-retry',
+	},
+
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
+
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
+		},
+
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] },
+		},
+	],
+});
