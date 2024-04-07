73038de36a9256000864c5d2bbfe209142b2b9cc
Remco Lakens <remco.lakens@gmail.com>
2024-04-07 15:09:57 +0200

    feat: add e2e tests (#76)
    ## Context
    
    This pull-request adds e2e tests as new feature to the boilerplate
    
    ## Changes
    
    - Added playwright config file
    - Installed dependencies
    - Configured Jest to ignore Playwright tests
    - Added example e2e test
    - Added GitHub Actions workflow
    - Adjusted the `.env.example`
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] I have tested my code for breaking changes and added the
    corresponding footer in this PR if needed
    - [x] I have added tests that prove my fix is effective or that my
    feature works

---
+++ e2e/example.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('test homepage', () => {
+	test('Find links in DOM', async ({ page }) => {
+		await page.goto('/');
+
+		await expect(
+			page.getByRole('link', { name: 'Docs -> Find in-depth' }),
+		).toBeVisible();
+		await expect(
+			page.getByRole('link', { name: 'Learn -> Learn about Next.js' }),
+		).toBeVisible();
+		await expect(
+			page.getByRole('link', { name: 'Templates -> Explore starter' }),
+		).toBeVisible();
+		await expect(
+			page.getByRole('link', { name: 'Deploy -> Instantly deploy' }),
+		).toBeVisible();
+	});
+});
--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 	testEnvironment: 'jsdom',
+	testPathIgnorePatterns: ['<rootDir>/e2e'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
