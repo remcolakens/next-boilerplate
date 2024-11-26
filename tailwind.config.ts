470d7ba491e6950147779ee6578bcfcc68100e3f
Remco Lakens <remco.lakens@gmail.com>
2024-11-26 17:00:56 +0100

    chore: dark mode class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+	darkMode: ['class'],
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
