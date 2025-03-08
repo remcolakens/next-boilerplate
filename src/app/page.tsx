52e9599a66ef791a7b7a9f24d78e3374183d4730
Remco Lakens <remco.lakens@gmail.com>
2025-03-08 23:52:12 +0100

    feat: update next & shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
 			<main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
 				<Image
 					className="dark:invert"
